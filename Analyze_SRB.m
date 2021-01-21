9876b33825b7ab4de4ec5fd3e8e9352d7f260863
MohamedNedal <mohamed.nedal.solar@gmail.com>
2021-01-21 21:25:50 +0200

    Adding comments and minor fixes

--- Analyze_SRB.m
+++ Analyze_SRB.m
@@ -1,38 +1,42 @@
 %% General Protocol for analyzing solar type-II radio bursts 
 % Written by: Mohamed Nedal 
-close all; clear; clc 
-%% 
-filename = input('Enter the FIT file name: \n', 's'); 
-A = fitsread(filename); 
-data_info = fitsinfo(filename); 
-date_obs = data_info.PrimaryData.Keywords{17,2}; 
-time_obs = data_info.PrimaryData.Keywords{18,2}; 
+% Link: https://github.com/MohamedNedal/RadioBurstAnalysis/tree/version_2 
 
-imagesc(flipud(A)) 
+close all % close all plots 
+clear     % clear the workspace 
+clc       % clear the command window
+%% 
+filename = input('Enter the FIT file name:\nFor example\nDARO_20130502_050001_58.fit\n\n', 's'); % prompt message 
+A = fitsread(filename);                               % import the FITS file 
+data_info = fitsinfo(filename);                  % read the header of the FITS file 
+date_obs = data_info.PrimaryData.Keywords{17,2}; % extract the observation date 
+time_obs = data_info.PrimaryData.Keywords{18,2}; % extract the observation time 
+A = flipud(A); % flip the data array to make the y-axis starts from 0 at x = 0 
+imagesc(A)     % show the data image 
 colormap('jet'); colorbar; grid on; grid(gca,'minor'); 
 set(gca,'YDir','normal'); set(gca,'XMinorTick','on','YMinorTick','on'); 
 ax = gca; 
 ax.XTick = [0,240,480,720,960,1200,1440,1680,1920,2160,2400,2640,2880,3120,3360,3600]; 
 ax.XTickLabel = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]; 
-title(data_info.PrimaryData.Keywords{12,2}); 
-xlabel(data_info.PrimaryData.Keywords{28,2}); 
-ylabel(data_info.PrimaryData.Keywords{32,2}); 
+title(data_info.PrimaryData.Keywords{12,2});  % extract the file name 
+xlabel(data_info.PrimaryData.Keywords{28,2}); % extract the label of x-axis 
+ylabel(data_info.PrimaryData.Keywords{32,2}); % extract the label of y-axis 
 
-s = input('Please enter "1" for single-band type-II burst, or enter "2" for type-II burst with bandsplitting: \n', 's'); 
-s = str2double(s); 
+s = input('\nPlease enter [1] for single-band type-II burst,\nor enter [2] for type-II burst with band-splitting:\n', 's'); 
+s = str2double(s); % convert the input number from string to integer 
 
 if s == 1
-    fprintf('Analyzing single-band type-II radio burst ... \n'); 
-    pointsNum = input('Enter the number of data points: \n'); 
+    fprintf('\nAnalyzing single-band type-II radio burst ...\n'); 
+    pointsNum = input('Enter the number of data points:\n'); 
     sprintf('The file datetime is: %s %s \n', ... 
         data_info.PrimaryData.Keywords{17,2}, data_info.PrimaryData.Keywords{18,2}) 
-
-    activity_deg = input("Enter the model's fold number (1 - 4; [1] for quiet Sun, [4] for active Sun): \n"); 
-    f_or_hr = input('Enter [1] for fundamental backbone, [2] for Harmonic backbone: \n'); 
+    % the fold number is just a factor multiplied with the equation to
+    % represent the degree of solar activity 
+    activity_deg = input("Enter the model's fold number (1 - 4; [1] for quiet Sun, [4] for active Sun):\n"); 
+    f_or_hr = input('\nEnter [1] for fundamental backbone, [2] for Harmonic backbone:\n'); 
     
     % REPEAT THE ANALYSIS FOR CALCULATING THE ERROR 
-    fprintf('For calculating the standard error ... \n') 
-    rpt = input('How many repetitions of the analysis? \n'); 
+    rpt = input('\nFor calculating the standard error,\nHow many repetitions for the analysis?\n'); 
     
     [output_arr, f] = callisto_single(filename, pointsNum, activity_deg, f_or_hr, rpt); 
     
@@ -46,7 +50,7 @@ if s == 1
     stderror_v = std(V') / sqrt(rpt); 
 
 elseif s == 2 
-    fprintf('Analyzing type-II radio burst with band-splitting ... \n') 
+    fprintf('\nAnalyzing type-II radio burst with band-splitting ... \n') 
     pointsNum = input('Enter the number of data points: \n'); 
 
     sprintf('The file datetime is: %s %s \n', ... 
@@ -56,11 +60,11 @@ elseif s == 2
     f_or_hr = input('Enter [1] for fundamental backbone, [2] for Harmonic backbone: \n');    
     
     % REPEAT THE ANALYSIS FOR CALCULATING THE ERROR 
-    fprintf('For calculating the standard error ... \n') 
-    rpt = input('How many repetitions of the analysis? \n'); 
+    rpt = input('\nFor calculating the standard error,\nHow many repetitions of the analysis?\n'); 
     
     [output_arr] = callisto_bandsplit(filename, pointsNum, activity_deg, f_or_hr, rpt); 
-
+    
+    % extract the shock parameters 
     X = [output_arr{:,1}]; 
     Ma = [output_arr{:,2}]; 
     BDW = [output_arr{:,3}]; 
@@ -76,6 +80,7 @@ elseif s == 2
     r2 = [output_arr{:,13}]; 
     t = [output_arr{:,14}]; 
     
+    % calculate the standard error for the shock parameters 
     stderror_X = std(X') / sqrt(rpt); 
     stderror_Ma = std(Ma') / sqrt(rpt); 
     stderror_BDW = std(BDW') / sqrt(rpt); 
@@ -89,7 +94,6 @@ elseif s == 2
     stderror_n2 = std(n2') / sqrt(rpt); 
     stderror_r1 = std(r1') / sqrt(rpt); 
     stderror_r2 = std(r2') / sqrt(rpt); 
-    
     stderror_dfdt = stderror_dfdt'; 
     stderror_Vs = stderror_Vs'; 
     stderror_r1 = stderror_r1'; 
@@ -105,10 +109,10 @@ elseif s == 2
     subplot(2,3,1) 
     yyaxis left 
     plot(t(:,1), f1(:,1)) 
-    ylabel('F-Plasma Frequency (MHz)') 
+    ylabel('LFB (MHz)') 
     yyaxis right 
     plot(t(:,1), f2(:,1)) 
-    ylabel('H-Plasma Frequency (MHz)') 
+    ylabel('HFB (MHz)') 
     xlabel('Time (s)') 
 
     subplot(2,3,2) 
@@ -126,20 +130,20 @@ elseif s == 2
     yyaxis left 
     plot(t(:,1), r1(:,1)) 
     errorbar(t(:,1), r1(:,1), stderror_r1(:,1)) 
-    ylabel('Height for F-band (Rsun)') 
+    ylabel('Height for LFB (Rsun)') 
     yyaxis right 
     plot(t(:,1), r2(:,1)) 
     errorbar(t(:,1), r2(:,1), stderror_r2(:,1)) 
-    ylabel('Height for H-band (Rsun)') 
+    ylabel('Height for HFB (Rsun)') 
     xlabel('Time (s)') 
 
     subplot(2,3,4) 
     yyaxis left 
     plot(r2(:,1), f1(:,1)) 
-    ylabel('F-Plasma Frequency (MHz)') 
+    ylabel('LFB (MHz)') 
     yyaxis right 
     plot(r2(:,1), f2(:,1)) 
-    ylabel('H-Plasma Frequency (MHz)') 
+    ylabel('HFB (MHz)') 
     xlabel('Height (Rsun)') 
 
     subplot(2,3,5) 
@@ -164,5 +168,5 @@ elseif s == 2
     xlabel('Height (Rsun)') 
 
 else 
-    fprintf(2, 'Wrong input! Try again ... \n', filename) 
+    fprintf(2, 'Wrong input! Try again ... \n') 
 end 
